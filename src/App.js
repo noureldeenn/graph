93708b6a0284c712867196ed611c0ebca5540ba7
noureldeenn <nourbadr4646@gmail.com>
2022-03-01 17:40:24 +0200

    add click event to conect nodes manual

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import Graph from "react-graph-vis";
-import React, { useState } from "react";
+import React, { useState, useEffect, useMemo, useCallback } from "react";
 
 const options = {
   layout: {
@@ -9,19 +9,6 @@ const options = {
     color: "#000000",
   },
 };
-
-function randomColor() {
-  const red = Math.floor(Math.random() * 256)
-    .toString(16)
-    .padStart(2, "0");
-  const green = Math.floor(Math.random() * 256)
-    .toString(16)
-    .padStart(2, "0");
-  const blue = Math.floor(Math.random() * 256)
-    .toString(16)
-    .padStart(2, "0");
-  return `#${red}${green}${blue}`;
-}
 
 const nodes = {
   levels: [
@@ -36,17 +23,17 @@ const nodes = {
     {
       id: 2,
       element: [
-        { id: 2.1, label: "Node 1.1", color: "#e09c41" },
-        { id: 2.2, label: "Node 1.2", color: "#e09c41" },
-        { id: 2.3, label: "Node 1.3", color: "#e09c41" },
+        { id: 2.1, label: "Node 2.1", color: "#e09c41" },
+        { id: 2.2, label: "Node 2.2", color: "#e09c41" },
+        { id: 2.3, label: "Node 2.3", color: "#e09c41" },
       ],
     },
     {
       id: 3,
       element: [
-        { id: 3.1, label: "Node 1.1", color: "#e0df41" },
-        { id: 3.2, label: "Node 1.2", color: "#e0df41" },
-        { id: 3.3, label: "Node 1.3", color: "#e0df41" },
+        { id: 3.1, label: "Node 3.1", color: "#e0df41" },
+        { id: 3.2, label: "Node 3.2", color: "#e0df41" },
+        { id: 3.3, label: "Node 3.3", color: "#e0df41" },
       ],
     },
   ],
@@ -58,59 +45,58 @@ const newNode = nodes.levels.reduce((acc, el) => {
   ]);
   return acc;
 }, []);
-const newEdge = nodes.levels.reduce((acc, el, index, self) => {
-  acc = acc.concat(el.element.map((l) => ({ from: el.id, to: l.id })));
-  if (self[index + 1]) {
-    acc = acc.concat({ from: el.id, to: self[index + 1].id });
-  }
-  return acc;
-}, []);
+// const newEdge = nodes.levels.reduce((acc, el, index, self) => {
+//   acc = acc.concat(el.element.map((l) => ({ from: el.id, to: l.id })));
+//   if (self[index + 1]) {
+//     acc = acc.concat({ from: el.id, to: self[index + 1].id });
+//   }
+//   return acc;
+// }, []);
 const App = () => {
-  const createNode = (x, y) => {
-    const color = randomColor();
-    setState(({ graph: { nodes, edges }, counter, ...rest }) => {
-      const id = counter + 1;
-      const from = Math.floor(Math.random() * (counter - 1)) + 1;
-      return {
-        graph: {
-          nodes: [...nodes, { id, label: `Node ${id}`, color, x, y }],
-          edges: [...edges, { from, to: id }],
-        },
-        counter: id,
-        ...rest,
-      };
-    });
-  };
-  const [state, setState] = useState({
-    counter: 5,
-    graph: {
-      nodes: newNode,
-      edges: newEdge,
-    },
-    events: {
-      select: ({ nodes, edges }) => {
-        console.log("Selected nodes:");
-        console.log(nodes);
-        console.log("Selected edges:");
-        console.log(edges);
-        alert("Selected node: " + nodes);
-      },
-      doubleClick: ({ pointer: { canvas } }) => {
-        createNode(canvas.x, canvas.y);
-      },
-    },
+  const [selectedItems, setSelectedItems] = useState([]);
+  const [graph, setGraph] = useState({
+    nodes: newNode,
+    edges: [],
   });
-  const { graph, events } = state;
+
+  const click = useCallback(({ nodes, edges }) => {
+    console.log("Selected nodes:", nodes);
+    console.log({ nodes });
+    setSelectedItems((prev) => [...prev, ...nodes]);
+    console.log("Selected edges:", edges);
+  }, []);
+  useEffect(() => {
+    if (selectedItems.length === 2) {
+      setGraph((prev) => ({
+        ...prev,
+        edges: [
+          ...prev.edges,
+          { from: selectedItems[0], to: selectedItems[1] },
+        ],
+      }));
+      setSelectedItems([]);
+    }
+  }, [selectedItems]);
+
+  const events = useMemo(
+    () => ({
+      click,
+    }),
+    [click]
+  );
+
   return (
-    <div>
-      <h1>React graph vis</h1>
-      <Graph
-        graph={graph}
-        options={options}
-        events={events}
-        style={{ height: "640px" }}
-      />
-    </div>
+    <>
+      <div>
+        <h1>React graph vis</h1>
+        <Graph
+          graph={graph}
+          options={options}
+          events={events}
+          style={{ height: "640px" }}
+        />
+      </div>
+    </>
   );
 };
 
